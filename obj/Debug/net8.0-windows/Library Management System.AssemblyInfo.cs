7406efc6ca941c0700b9fb99eef9e903ec6a1d40
Essam Ayman <essamayman111@gmail.com>
2024-02-12 06:52:57 +0200

    initialize

--- obj/Debug/net8.0-windows/Library Management System.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Library Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4656b3c9e25491266324850983b49bd4cd630c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc098a0a978692416f7c6cfb311dbf0039481ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
